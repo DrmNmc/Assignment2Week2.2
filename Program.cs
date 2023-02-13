f62da0179b4846db6076c6745c91f479d9bc3a8f
Dylan Miller <103227705+jibbajab@users.noreply.github.com>
2023-02-12 23:27:41 -0500

    init

--- Program.cs
+++ Program.cs
@@ -1,85 +1,49 @@
 ﻿using System;
 
-namespace FunWithMusic
+namespace myParams
 {
-    enum MusicGenre
-    {
-        Rock,
-        Pop,
-        HipHop,
-        Jazz,
-        Classical
-    }
-
-    struct Music
-    {
-        public string Title;
-        public string Artist;
-        public string Album;
-        public int Length;
-        public MusicGenre Genre;
-
-        public Music(string title, string artist, string album, int length, MusicGenre genre)
-        {
-            Title = title;
-            Artist = artist;
-            Album = album;
-            Length = length;
-            Genre = genre;
-        }
-
-        public void Display()
-        {
-            Console.WriteLine("Title: {0}", Title);
-            Console.WriteLine("Artist: {0}", Artist);
-            Console.WriteLine("Album: {0}", Album);
-            Console.WriteLine("Length: {0} seconds", Length);
-            Console.WriteLine("Genre: {0}", Genre);
-        }
-
-        public void setTitle(string title)
-        {
-            Title = title;
-        }
-
-        public void setLength(int length)
-        {
-            Length = length;
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the title of the song: ");
-            string tempTitle = Console.ReadLine();
-            Console.Write("Enter the artist of the song: ");
-            string tempArtist = Console.ReadLine();
-            Console.Write("Enter the album of the song: ");
-            string tempAlbum = Console.ReadLine();
-            Console.Write("Enter the length of the song in seconds: ");
-            int tempLength = int.Parse(Console.ReadLine());
-            Console.Write("Enter the genre of the song (Rock, Pop, HipHop, Jazz, or Classical): ");
-            MusicGenre tempGenre = (MusicGenre)Enum.Parse(typeof(MusicGenre), Console.ReadLine());
-
-            Music music = new Music(tempTitle, tempArtist, tempAlbum, tempLength, tempGenre);
+            Random rand = new Random();
+            Console.Write("How many values do you want to generate? ");
+            int size = int.Parse(Console.ReadLine());
+            int[] numbers = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                numbers[i] = rand.Next(1, 11);
+            }
+            Console.WriteLine("The random numbers are:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
             Console.WriteLine();
-            Console.WriteLine("First song:");
-            music.Display();
-            Console.WriteLine();
+            int totalAdd = Add(numbers);
+            Console.WriteLine("The sum of the numbers is " + totalAdd);
+            int totalMultiply = Multiply(numbers);
+            Console.WriteLine("The product of the numbers is " + totalMultiply);
+        }
 
-            Music moreMusic = music;
-            moreMusic.setTitle("Strawberry Jelly Forever");
-            moreMusic.setLength(222);
+        static int Add(params int[] numbers)
+        {
+            int total = 0;
+            foreach (int num in numbers)
+            {
+                total += num;
+            }
+            return total;
+        }
 
-            Console.WriteLine("First song:");
-            music.Display();
-            Console.WriteLine();
-            Console.WriteLine("Second song:");
-            moreMusic.Display();
-
-            Console.ReadLine();
+        static int Multiply(params int[] numbers)
+        {
+            int total = 1;
+            foreach (int num in numbers)
+            {
+                total *= num;
+            }
+            return total;
         }
     }
 }
