8a6106fed233aaefc7c9b3f315ec6c397332ccab
Dylan Miller <103227705+jibbajab@users.noreply.github.com>
2023-02-12 23:31:52 -0500

    init

--- Program.cs
+++ Program.cs
@@ -1,49 +1,100 @@
 ﻿using System;
 
-namespace myParams
+namespace MusicArray
 {
+    enum MusicGenre
+    {
+        Rock,
+        Pop,
+        HipHop,
+        Jazz,
+        Classical
+    }
+
+    struct Music
+    {
+        public string Title;
+        public string Artist;
+        public string Album;
+        public int Length;
+        public MusicGenre Genre;
+
+        public void setTitle(string title)
+        {
+            Title = title;
+        }
+
+        public void setArtist(string artist)
+        {
+            Artist = artist;
+        }
+
+        public void setAlbum(string album)
+        {
+            Album = album;
+        }
+
+        public void setGenre(MusicGenre genre)
+        {
+            Genre = genre;
+        }
+
+        public string Display()
+        {
+            return $"Title: {Title}\nArtist: {Artist}\nAlbum: {Album}\nLength: {Length} seconds\nGenre: {Genre}\n";
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Random rand = new Random();
-            Console.Write("How many values do you want to generate? ");
+            Console.WriteLine("How many songs would you like to enter?");
             int size = int.Parse(Console.ReadLine());
-            int[] numbers = new int[size];
-            for (int i = 0; i < size; i++)
-            {
-                numbers[i] = rand.Next(1, 11);
-            }
-            Console.WriteLine("The random numbers are:");
-            for (int i = 0; i < size; i++)
-            {
-                Console.Write(numbers[i] + " ");
-            }
-            Console.WriteLine();
-            int totalAdd = Add(numbers);
-            Console.WriteLine("The sum of the numbers is " + totalAdd);
-            int totalMultiply = Multiply(numbers);
-            Console.WriteLine("The product of the numbers is " + totalMultiply);
-        }
+            Music[] collection = new Music[size];
 
-        static int Add(params int[] numbers)
-        {
-            int total = 0;
-            foreach (int num in numbers)
+            try
             {
-                total += num;
-            }
-            return total;
-        }
+                for (int i = 0; i < size; i++)
+                {
+                    Console.WriteLine($"Song {i + 1}");
+                    Console.WriteLine("Please enter the song title:");
+                    collection[i].setTitle(Console.ReadLine());
 
-        static int Multiply(params int[] numbers)
-        {
-            int total = 1;
-            foreach (int num in numbers)
-            {
-                total *= num;
+                    Console.WriteLine("Please enter the artist name:");
+                    collection[i].setArtist(Console.ReadLine());
+
+                    Console.WriteLine("Please enter the album name:");
+                    collection[i].setAlbum(Console.ReadLine());
+
+                    Console.WriteLine("Please enter the length of the song in seconds:");
+                    collection[i].Length = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Please enter the genre of the song (Rock, Pop, HipHop, Jazz, or Classical):");
+                    collection[i].setGenre((MusicGenre)Enum.Parse(typeof(MusicGenre), Console.ReadLine()));
+
+                    Console.WriteLine();
+                }
             }
-            return total;
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    Console.WriteLine(collection[i].Display());
+                }
+            }
         }
     }
 }
